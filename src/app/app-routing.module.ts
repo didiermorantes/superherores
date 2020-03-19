c53b0a6bbe1499431051b923f06e4de48dabfd06
Morantes Sanchez <e.morantessanchez@dxc.com>
2020-03-18 21:29:46 -0500

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home' , component: HomeComponent},
+  {path: 'about' , component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
